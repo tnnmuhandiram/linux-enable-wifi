f6d611204663e6c98cb531955dc640e4927234c0
Tharindu Muhandiram <44857250+tnnmuhandiram@users.noreply.github.com>
2025-01-30 03:52:16 +0530

    Update bt_advertise.sh

--- bt_advertise.sh
+++ bt_advertise.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
+# Enable Bluetooth and configure settings
 echo -e "power on\ndiscoverable on\npairable on\nadvertise on\nsystem-alias BitzifyGateway" | bluetoothctl
 
+# Wait for a moment to ensure settings take effect
+sleep 1
+
+# Start advertising a service UUID (replace UUID with your actual service UUID)
+SERVICE_UUID="00001101-0000-1000-8000-00805F9B34FB"
+
+echo -e "advertise uuids $SERVICE_UUID" | bluetoothctl
